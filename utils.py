d4f2993ca3452c01f6d1b03458089513472235d2
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-04-18 22:43:04 +0300

    file.writelines(files)

--- utils.py
+++ utils.py
@@ -165,7 +165,7 @@ def check_if_empty():
     if len(sorted_list()[0]) == 0:
         previous_ratings = parse()
         file = open('ratings.txt','w')
-        file.writelines(file)
+        file.writelines(previous_ratings)
         file.close()
 
 def save_to_cloud(start_at):

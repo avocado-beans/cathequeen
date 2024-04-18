a1035a2779fc4f4f2872b48e6b230a41d3fd24c3
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-04-18 18:04:50 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -127,7 +127,7 @@ def check_if_empty():
     if len(sorted_list()[0]) == 0:
         previous_ratings = requests.get('https://worker-8fat.onrender.com/get')
         file = open('ratings.txt','w')
-        file.writelines(file)
+        file.writelines(previous_ratings)
         file.close()
 
 def save_to_cloud(start_at):
